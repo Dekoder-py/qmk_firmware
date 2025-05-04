d86e549762b77780d9240eb0b9d14eacc5542a96
Dekoder-py <kyleyesmariam@icloud.com>
2025-05-04 14:45:02 +1200

    add autocorrect and caps word

--- keyboards/keychron/v1/ansi_encoder/keymaps/dekoder_py/rules.mk
+++ keyboards/keychron/v1/ansi_encoder/keymaps/dekoder_py/rules.mk
@@ -1,3 +1,5 @@
 ENCODER_MAP_ENABLE = yes
 VIA_ENABLE = yes		 # Enable VIA support
 RGB_MATRIX_ENABLE = yes  # Enable lighting
+CAPS_WORD_ENABLE = yes   # Enable Caps Word
+AUTOCORRECT_ENABLE = yes # Enable Autocorrect
