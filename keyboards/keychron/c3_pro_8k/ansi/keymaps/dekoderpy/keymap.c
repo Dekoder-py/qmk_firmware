89d7d1fae803b44a2b1d5d6dd292ab325b296ab5
Dekoder-py <kyle@codingcorner.dev>
2026-03-26 19:07:19 +1300

    add custom keycodes for hyper f and screenshot

--- keyboards/keychron/c3_pro_8k/ansi/keymaps/dekoderpy/keymap.c
+++ keyboards/keychron/c3_pro_8k/ansi/keymaps/dekoderpy/keymap.c
@@ -24,13 +24,18 @@ enum layers {
     WIN_FN,
 };
 
+enum custom_keycodes_user {
+    HYPER_F = NEW_SAFE_RANGE,
+    SCREENSHOT,
+};
+
 #define FN_MAC MO(MAC_FN)
 #define FN_WIN MO(WIN_FN)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_ansi(
-        KC_ESC,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_SNAP,  KC_SIRI,  KC_CAPS,
+        KC_ESC,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   SCREENSHOT,  KC_SIRI,  KC_CAPS,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,  KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
         KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
@@ -38,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, KC_ROPTN,  FN_MAC,  KC_RCTL,KC_LEFT,    KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_tkl_ansi(
-        _______,             KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, UG_VALD,  UG_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,   KC_VOLD, KC_VOLU,  _______,  _______,  _______,
+        _______,             KC_BRID,  KC_BRIU,  KC_MCTRL, HYPER_F, UG_VALD,  UG_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,   KC_VOLD, KC_VOLU,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         UG_TOGG,  UG_NEXT,  UG_VALU,  UG_HUEU,  UG_SATU,  UG_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         OS_TOGGL, UG_PREV,  UG_VALD,  UG_HUED,  UG_SATD,  UG_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
@@ -61,6 +66,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
         _______,  GU_TOGG,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case HYPER_F:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                register_code(KC_LCMD);
+                register_code(KC_LOPT);
+                register_code(KC_LSFT);
+                register_code(KC_F);
+            } else {
+                unregister_code(KC_F);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_LOPT);
+                unregister_code(KC_LCMD);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case SCREENSHOT:
+            if (record->event.pressed) {
+                register_code(KC_LCMD);
+                register_code(KC_LSFT);
+                register_code(KC_5);
+            } else {
+                unregister_code(KC_5);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_LCMD);
+            }
+            return false;
+    }
+    return true;
+}
 
 void keyboard_post_init_user(void) {
     set_single_persistent_default_layer(MAC_BASE_LAYER);
